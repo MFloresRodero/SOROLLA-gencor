eeb1707a08d86d552771a3f9df0ce0c27d26808a
MFloresRodero <maria.flores@bsc.es>
2025-02-05 16:41:43 +0100

    This file has been modified to incorporate a couple of lines of code so when it's created it will change the extension of some files, this is because several files need to be manually processed and could not be done inside a script

--- scripts/pipeline/01_from_excell_catalog_to_final_csv.py
+++ scripts/pipeline/01_from_excell_catalog_to_final_csv.py
@@ -4,16 +4,20 @@ import yaml
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="This script takes the data from the catalog and transform it into a data description file defining all the paths and whether the data has been run through certain softwares. It also adds an N column by default in case there is not one.")
-    parser.add_argument("--env", choices=["local", "remote"], required=True,
-                        help="Specify if you are running this file in local (local) or in the MN5 (remote)")
+    parser = argparse.ArgumentParser(description="This script takes the data from the catalog and transform it into a data description file defining all the paths and whether the data has been run through certain softwares.")
+    parser.add_argument("--env", choices=["local", "remote", "shared"], required=True,
+                        help="Specify if you are running this file in local (local) or in the MN5 (remote, shared)")
     args = parser.parse_args()
 
     # Load data based on environment
     if args.env == "local":
         config_file = "/home/maria/git/SOROLLA/config/config.yaml"
-    else:
+    elif args.env == "remote":
         config_file = "/gpfs/projects/bsc02/mflores/gencor/config/config.yaml"
+    elif args.env == "shared":
+        config_file = "/gpfs/projects/hpcsharing/364592/config/config.yaml"
+    else:
+        raise ValueError("Environment not found, check if you specified the environment with --env or if the path is wrong")
 
     # Load configuration file securely
     try:
@@ -31,7 +35,8 @@ def main():
     sumstats_folder = config["SumStats"]["sumstats_folder"]
     raw_catalog = config["SumStats"]["raw_catalog"]
     description_csv = config["SumStats"]["description_csv"]
-    raw_folder = config["SumStats"]["raw_folder"]
+    # raw_folder = config["SumStats"]["raw_folder"]
+    preprocessed_folder = config["SumStats"]["preprocessed_folder"]
 
     # Construct paths
     raw_catalog_path = os.path.join(base_path, sumstats_folder, raw_catalog)
@@ -41,12 +46,13 @@ def main():
                        "ref_genome", "snp", "a1", "a2", "frq", "FRQ_U", "FRQ_A", 
                        "z", "b", "OR", "se", "p", "N_col", "N_num", "Nca_col", 
                        "Nca_val", "Nco_col", "Nco_val", "INFO", "ignore"]
+
     
     # Call create_and_append_dataframe function with appropriate arguments
     create_and_append_dataframe(raw_catalog_path, description_csv_path, desired_columns, config)
 
 
-def create_and_append_dataframe(input_file, output_file, desired_columns, config, munged_default="False", wrangled_default="False", munged_path_default="NA", wrangled_path_default="NA"):
+def create_and_append_dataframe(input_file, output_file, desired_columns, config, munged_default="False", wrangled_default="False", munged_path_default=None, wrangled_path_default=None):
     """
     Creates and appends data to a pandas DataFrame.
 
@@ -54,10 +60,10 @@ def create_and_append_dataframe(input_file, output_file, desired_columns, config
       input_file (str): Path to the input CSV file.
       output_file (str): Path to the output CSV file.
       desired_columns (list): List of column names to keep from the input CSV.
-      munged_default (str, optional): Default value for the 'munged' column ("False" by default).
-      wrangled_default (str, optional): Default value for the 'wrangled' column ("False" by default).
-      munged_path_default (str, optional): Default value for the 'munged_path' column ("NA" by default).
-      wrangled_path_default (str, optional): Default value for the 'wrangled_path' column ("NA" by default).
+      munged_default (str, optional): Default value for the 'munged' column, None.
+      wrangled_default (str, optional): Default value for the 'wrangled' column, None.
+      munged_path_default (str, optional): Default value for the 'munged_path' column, None.
+      wrangled_path_default (str, optional): Default value for the 'wrangled_path' column, None.
     """
 
     # Check if input file exists
@@ -82,16 +88,36 @@ def create_and_append_dataframe(input_file, output_file, desired_columns, config
     df['Nca_val'] = df['Nca_val'].apply(lambda x: str(int(float(x))) if pd.notna(x) else x)
     df['Nco_val'] = df['Nco_val'].apply(lambda x: str(int(float(x))) if pd.notna(x) else x)
 
+    for index, row in df.iterrows():
+        if "vcf" in row["filename"]:
+            df.at[index, "filename"] = row["filename"].replace(".vcf","")
+        elif "zip" in row["filename"]:
+            df.at[index, "filename"] = row["filename"].replace("zip","txt")
+        else:
+            continue
+
+    # Separate the standard error columns from b and OR
+    df.rename(columns={"se":"se_beta"}, inplace=True)
+    df["se_OR"] = None
+    df["se_OR"] = df.apply(lambda row: row["se_beta"] if pd.isna(row["b"]) and pd.notna(row["OR"]) else None, axis=1)
+    df["se_beta"] = df.apply(lambda row: None if pd.isna(row["b"]) and pd.notna(row["OR"]) else row["se_beta"], axis=1)
+
 
     # Add new columns with defaults
     df['local_raw_path'] = df.apply(lambda row: os.path.join(config['local']['base_path'], config['SumStats']['raw_folder'], row['filename']), axis=1)
     df['remote_raw_path'] = df.apply(lambda row: os.path.join(config['remote']['base_path'], config['SumStats']['raw_folder'], row['filename']), axis=1)
+    df['shared_raw_path'] = df.apply(lambda row: os.path.join(config['shared']['base_path'], config['SumStats']['raw_folder'], row['filename']), axis=1)
+    df['local_preprocessed_path'] = df.apply(lambda row: os.path.join(config['local']['base_path'], config['SumStats']['preprocessed_folder'], f"{row['label']}_{row['id']}.tsv"), axis=1) 
+    df['remote_preprocessed_path'] = df.apply(lambda row: os.path.join(config['remote']['base_path'], config['SumStats']['preprocessed_folder'], f"{row['label']}_{row['id']}.tsv"), axis=1)
+    df['shared_preprocessed_path'] = df.apply(lambda row: os.path.join(config['shared']['base_path'], config['SumStats']['preprocessed_folder'], f"{row['label']}_{row['id']}.tsv"), axis=1)
     df['munged'] = munged_default
     df['local_munged_path'] = munged_path_default
     df['remote_munged_path'] = munged_path_default
+    df['shared_munged_path'] = munged_path_default
     df['wrangled'] = wrangled_default
     df['local_wrangled_path'] = wrangled_path_default
     df['remote_wrangled_path'] = wrangled_path_default
+    df['shared_wrangled_path'] = wrangled_path_default
 
     # Handle existing IDs (assuming 'id' is the unique identifier)
     if (os.path.exists(output_file)):
@@ -101,6 +127,20 @@ def create_and_append_dataframe(input_file, output_file, desired_columns, config
             df = df.loc[~df['id'].isin(existing_ids)]  # Filter out existing IDs
         except FileNotFoundError:
             pass  # No existing file, proceed normally
+
+    
+    # Order the columns:
+    ordered_columns = ["type", "selection", "id","disease", "disease_subtype", "label", "filename", 
+                       "ref_genome", "snp", "a1", "a2", "frq", 
+                       "z", "b", "OR", "se_beta", "se_OR", "p", "N_col", "N_num", "Nca_col", 
+                       "Nca_val", "Nco_col", "Nco_val", "INFO", "ignore",
+                       "local_raw_path", "remote_raw_path", "shared_raw_path", 
+                       "local_preprocessed_path", "remote_preprocessed_path", "shared_preprocessed_path",
+                       "munged", "local_munged_path", "remote_munged_path", "shared_munged_path",
+                       "wrangled", "local_wrangled_path", "remote_wrangled_path", "shared_wrangled_path",
+                       ]
+
+    df = df[ordered_columns]
 
     # Append data to output file (create if it doesn't exist)
     df.to_csv(output_file, mode='a', header=not os.path.exists(output_file), index=False)
