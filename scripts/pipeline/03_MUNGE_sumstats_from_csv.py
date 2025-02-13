bbc616f1021037cad1e3e14efa2c2d32349c3dea
MFloresRodero <maria.flores@bsc.es>
2025-02-13 17:06:14 +0100

    The WRANGLE and MUNGE scripts have been changed to incorporate the use of preprocessed data instead of just using the RAW data and the columns described by the description_sumstats.csv, instead not they rely on whether the columns are present or not according to the data_description_matrix.csv generated during the preprocessing (script number 02)

--- scripts/pipeline/03_MUNGE_sumstats_from_csv.py
+++ scripts/pipeline/03_MUNGE_sumstats_from_csv.py
@@ -6,11 +6,20 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="This script automatises and runs munge_sumstats.py")
-    parser.add_argument("--env", choices=["local", "remote"], required=True,
+    parser.add_argument("--env", choices=["local", "remote", "shared"], required=True,
                         help="Specify if you are running this file in local (local) or in the MN5 (remote)")
     args = parser.parse_args()
+
     # Load data based on environment
-    config_file = "/home/maria/git/SOROLLA/config/config.yaml" if args.env == "local" else "/gpfs/projects/bsc02/mflores/gencor/config/config.yaml"
+    if args.env == "local":
+        config_file = "/home/maria/git/SOROLLA/config/config.yaml"
+    elif args.env == "remote":
+        config_file = "/gpfs/projects/bsc02/mflores/gencor/config/config.yaml"
+    elif args.env == "shared":
+        config_file = "/gpfs/projects/hpcsharing/364592/config/config.yaml"
+    else:
+        raise ValueError("Environment not found, check if you specified the environment with --env or if the path is wrong")
+
 
     # Load configuration file securely
     try:
@@ -27,208 +36,115 @@ def main():
     base_path = config[args.env]["base_path"]
     print(f"Base path: {base_path}")
 
+    # Configuration files
     sumstats_folder = config["SumStats"]["sumstats_folder"]
     description_csv = config["SumStats"]["description_csv"]
-    output_folder = config["SumStats"]["munged_folder"]
+    munged_folder = config["SumStats"]["munged_folder"]
     munge_script = config["Scripts"]["ldsc"]["ldsc_munge"]
     singularity_env = config[args.env]["environments_folder"]
     singularity_file = config[args.env]["singularity_ldsc"]
     merge_alleles = config["reference_genomes"]["reference_genomes_folder"]
     merge_alleles_map = config["reference_genomes"]["HapMap3"]
+    matrix_description = config["SumStats"]["matrix_description"]
 
-    # print(f"description_csv: {description_csv}")
+    # Build paths
+    output_folder = os.path.join(base_path, munged_folder)
     description_csv_path = os.path.join(base_path, sumstats_folder, description_csv)
-    # print(f"description_csv_path: {description_csv_path}")
-
-    # print(f"munge_script: {munge_script}")
     munge_script_path = os.path.join(base_path, munge_script)
-    # print(f"munge_script_path: {munge_script_path}")
-
-    # print(f"singularity_env: {singularity_env}")
-    # print(f"singularity_ldsc: {config[args.env]['singularity_ldsc']}")
     singularity_env_path = os.path.join(singularity_env, singularity_file)
-    # print(f"Singularity_env_path: {singularity_env_path}")
-
-    # print(f"merge_alleles: {merge_alleles}")
-    # print(f"HapMap3: {config['reference_genomes']['HapMap3']}")
     merge_alleles_path = f"{base_path}{merge_alleles}{merge_alleles_map}"
-    #merge_alleles_path = os.path.join(base_path, merge_alleles, merge_allele_map)
-    # print(f"Merge allele path: {merge_alleles_path}")
-    # raise
+    matrix_csv_path = os.path.join(base_path, sumstats_folder, matrix_description)
 
     # Call function to run munge_sumstats
-    run_munge_sumstats(description_csv_path, args.env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder)
+    run_munge_sumstats(description_csv_path, matrix_csv_path, args.env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder)
 
-def generate_munge_command(row, env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder):
+def generate_munge_command(csv_row, matrix_row, env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder):
     print("Command is being generated")
     command = ["singularity", "exec", singularity_env_path, "python2", munge_script_path]
-    raw_path_column = f"{env}_raw_path"
-    command.extend(["--sumstats", str(row[raw_path_column])])
+    preprocessed_path_column = f"{env}_preprocessed_path"
+    command.extend(["--sumstats", str(csv_row[preprocessed_path_column])])
+    command.extend(["--snp", "rsID"])
+    command.extend(["--a1", "A1"])
+    command.extend(["--a2", "A2"])
+    command.extend(["--N", str(csv_row["N_num"])])
 
-    if not pd.isna(row["snp"]):
-        command.extend(["--snp", str(row["snp"])])
+    if matrix_row["A1_frequency"]:
+        command.extend(["--frq", "A1_frequency"])
 
-    if not pd.isna(row["a1"]):
-        command.extend(["--a1", str(row["a1"])])
-
-    if not pd.isna(row["a2"]):
-        command.extend(["--a2", str(row["a2"])])
-
-    if not pd.isna(row["frq"]):
-        command.extend(["--frq", str(row["frq"])])
-
-    if not pd.isna(row["b"]):
-        command.extend(["--signed-sumstats", f"{str(row['b'])},0"])
-    elif not pd.isna(row["OR"]):
-        command.extend(["--signed-sumstats", f"{str(row['OR'])},1"])
-    elif not pd.isna(row["z"]):
-        command.extend(["--signed-sumstats", f"{str(row['z'])},0"])
-
-    if pd.isna(row["b"]) and pd.isna(row["z"]) and pd.isna(row["OR"]):
+    if matrix_row["beta"] in [1, 2]:
+        command.extend(["--signed-sumstats", f"beta,0"])
+    elif matrix_row["odds_ratio"] in [1, 2]:
+        command.extend(["--signed-sumstats", f"odds_ratio,1"])
+    elif matrix_row["zscore"] in [1, 2]:
+        command.extend(["--signed-sumstats", f"zscore,0"])
+    else:
         command.extend(["--a1-inc"])
-
-    if not pd.isna(row["p"]):
-        command.extend(["--p", str(row["p"])])
-
-    command.extend(["--N", str(row["N_num"])])
+        
+    if matrix_row["p_value"] in [1, 2]:
+        command.extend(["--p", "p_value"])
 
     # Additional metrics
     print("Additional metrics")
 
-    if not pd.isna(row["N_col"]):
-        command.extend(["--N-col", str(row["N_col"])])
+    if matrix_row["Ncol"] in [1, 2]:
+        command.extend(["--N-col", "Ncol"])
+        
+    if matrix_row["Nca_col"] in [1, 2]:
+        command.extend(["--N-cas-col", "Nca_col"])
+    if not pd.isna(csv_row["Nca_val"]):
+        command.extend(["--N-cas", str(csv_row["Nca_val"])])
+    
+    if matrix_row["Nco_col"] in [1, 2]:
+        command.extend(["--N-con-col", "Nco_col"])
+    if not pd.isna(csv_row["Nco_val"]):
+        command.extend(["--N-con", str(csv_row["Nco_val"])])
 
-    if not pd.isna(row["Nca_val"]):
-        command.extend(["--N-cas", str(row["Nca_val"])])
-
-    if not pd.isna(row["Nca_col"]):
-        command.extend(["--N-cas-col", str(row["Nca_col"])])
-
-    if not pd.isna(row["Nco_val"]):
-        command.extend(["--N-con", str(row["Nco_val"])])
-
-    if not pd.isna(row["Nco_col"]):
-        command.extend(["--N-con-col", str(row["Nco_col"])])
-
-    if not pd.isna(row["INFO"]):
-        command.extend(["--info", str(row["INFO"])])
+    if matrix_row["INFO"] in [1, 2]:
+        command.extend(["--info", "INFO"])
 
     print("Generate output path")
-
-    output_path = os.path.join(output_folder, f"{row['id']}_{row['label']}")
+    print(f"Output folder: {output_folder}")
+    output_path = os.path.join(output_folder, f"{matrix_row['label']}_{matrix_row['id']}")
+    print(f"Output path: {output_path}")
     command.extend(["--out", output_path])
 
     command.extend(["--chunksize", "500000"])
     command.extend(["--merge-alleles", merge_alleles_path])
-
-    # # Additional ignore flags based on column presence
-    # if not pd.isna(row["p"]):
-    #     print(f"Ignoring z because p-value is present in the row.")
-    #     command.append("--ignore")
-    #     command.append("z")
-
-    # if not pd.isna(row["ignore"]):
-    #     ignore_values = row["ignore"].split(',')
-    #     for ignore_value in ignore_values:
-    #         command.append("--ignore")
-    #         command.append(ignore_value.strip())  # Remove leading/trailing spaces
-
-    # return command
-    print("Start ignore command")
-    ignore_columns = []
-
-    if "p" in row and not pd.isna(row["p"]):
-        print(f"Ignoring z because p-value is present in the row.")
-        if not pd.isna(row["z"]):
-            ignore_columns.append(str(row["z"]))
-
-    # print("Error 1")
-
-    if row["p"] == "P_noSPA":
-        ignore_columns.append("p_value")
-        
-    # print("Error 2")
-    
-    if not pd.isna(row["ignore"]):
-        ignore_columns.append(str(row["ignore"]).replace(" ", ""))
-        
-    # print("Error 3")
-
-    if ignore_columns:
-        ignore_argument = ",".join(ignore_columns)
-        command.extend(["--ignore", ignore_argument])
-
-    # # # return command
-    # # print("Start ignore command")
-    # # ignore_columns = ""
-
-    # # if "p" in row and not pd.isna(row["p"]):
-    # #     print(f"Ignoring z because p-value is present in the row.")
-    # #     ignore_columns += str(row["z"])
-
-    # # print("Error 1")
-
-    # # if row["p"] == "P_noSPA":
-    # #     # ignore_columns += ",p_value"
-    # #     ignore_columns += ","
-    # #     ignore_columns += "p_value"
-        
-    # # print("Error 2")
-    
-    # # if not pd.isna(row["ignore"]):
-    # #     if ignore_columns:
-    # #         ignore_columns += ","
-        
-    # # print("Error 3")
-
-    # # if ignore_columns:
-    # #     ignore_columns += str(row["ignore"]).replace(" ","")
-    # #     command.extend(["--ignore", ignore_columns])
-
-        # for ignore_value in ignore_values:
-        #     ignore_columns.append(ignore_value.strip())  # Remove leading/trailing spaces
-
-        # for ignore_column in ignore_columns:
-        #     command.extend(["--ignore", ignore_column])
-        #if ignore_columns:
-        #     command.append("--ignore")
-        # #command.append(",".join(ignore_columns))
-
-            # command.append(ignore_column)
-    print("These columns have been ignored:")
-    print(ignore_columns) 
+    print(command)
     return command
 
-def run_munge_sumstats(csv_file, env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder):
-    data = pd.read_csv(csv_file)
-    from shlex import join
-    for index, row in data.iterrows():
-        # Skip rows where 'munged' is already True
+def run_munge_sumstats(csv_file, matrix_csv, env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder):
+
+    description_csv = pd.read_csv(csv_file)
+    matrix_data = pd.read_csv(matrix_csv)
+
+    for index, row in description_csv.iterrows():
         if str(row["munged"]) != "True":
             try:
-                command = generate_munge_command(row, env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder)
+                matrix_row = matrix_data.loc[matrix_data["id"]==row["id"]].iloc[0]
+                command = generate_munge_command(row, matrix_row, env, singularity_env_path, munge_script_path, merge_alleles_path, output_folder)
                 print("Command was generated")
                 print(f"\tCommand: \n{command}")
                 subprocess.run(command, check=True)
 
-                data.at[index, "munged"] = True
-                munged_path = os.path.join(output_folder, f"{row['id']}_{row['label']}.sumstats.gz")
-                data.at[index, f"{env}_munged_path"] = munged_path
-
+                description_csv.at[index, "munged"] = True
+                munged_path = os.path.join(output_folder, f"{row['label']}_{row['id']}.sumstats.gz")
+                description_csv.at[index, f"{env}_munged_path"] = munged_path
+                
             except subprocess.CalledProcessError as e:
                 print(f"Error processing row {index}: {e}")
-                data.at[index, "munged"] = "Error"
+                description_csv.at[index, "munged"] = "Error"
 
             except Exception as e:
                 print(f"Unexpected error processing row {index}: {e}")
-                data.at[index, "munged"] = "Error"
+                description_csv.at[index, "munged"] = "Error"
 
             finally:
-                data.to_csv(csv_file, index=False)
-
+                # Save updated CSV file
+                description_csv.to_csv(csv_file, index=False)
+            
         else:
-            print(f"{row['id']}_{row['label']} skipped, already munged")   
-
+            print(f"✅ {row['label']}_{row['label']} skipped, already munged.")
 
 if __name__ == "__main__":
     main()
